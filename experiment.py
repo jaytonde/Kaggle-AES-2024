aa73f6dd678dfea8f43953b2d2822c2d90d880b1
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-05-12 11:27:02 +0000

    lr change

--- experiment.py
+++ experiment.py
@@ -22,9 +22,9 @@ def prepare_dataset(config):
 
 def get_model(config):
 
-    tokenizer = AutoTokenizer.from_pretrained(config.model_id)
-    model_config    = AutoConfig.from_pretrained(config.model_id,num_labels=config.num_labels)
-    model     = AutoModelForSequenceClassification.from_pretrained(config.model_id,config=model_config)
+    tokenizer       = AutoTokenizer.from_pretrained(config.model_id)
+    model_config    = AutoConfig.from_pretrained(config.model_id, num_labels=config.num_labels)
+    model           = AutoModelForSequenceClassification.from_pretrained(config.model_id, config=model_config)
 
     return tokenizer, model
 
@@ -47,37 +47,36 @@ def main(config):
 
     if config.wandb_log:
         wandb.init(
-            project=config.wandb_project_name,
-            group=config.experiment_name,
-            name=f"{config.experiment_name}_fold_{config.fold}",
-            notes=config.notes,
-            config=OmegaConf.to_container(config, resolve=True),
-        )
+                        project = config.wandb_project_name,
+                        group   = config.experiment_name,
+                        name    = f"{config.experiment_name}_fold_{config.fold}",
+                        notes   = config.notes,
+                        config  = OmegaConf.to_container(config, resolve=True)
+                   )
 
     
     dataset           = prepare_dataset(config)
 
-    train_dataset = dataset.filter(lambda example: example["fold"] != config.fold)
-    eval_dataset  = dataset.filter(lambda example: example["fold"] == config.fold)
+    train_dataset     = dataset.filter(lambda example: example["fold"] != config.fold)
+    eval_dataset      = dataset.filter(lambda example: example["fold"] == config.fold)
 
     tokenizer, model  = get_model(config)
 
-    train_dataset = train_dataset.map(tokenize_function, batched=True, fn_kwargs={'tokenizer':tokenizer,'truncation':config.truncation,'max_length':config.max_length})
-    eval_dataset  = eval_dataset.map(tokenize_function, batched=True, fn_kwargs={'tokenizer':tokenizer,'truncation':config.truncation,'max_length':config.max_length})
+    train_dataset     = train_dataset.map(tokenize_function, batched=True, fn_kwargs={'tokenizer':tokenizer,'truncation':config.truncation,'max_length':config.max_length})
+    eval_dataset      = eval_dataset.map(tokenize_function, batched=True, fn_kwargs={'tokenizer':tokenizer,'truncation':config.truncation,'max_length':config.max_length})
 
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    data_collator     = DataCollatorWithPadding(tokenizer=tokenizer)
 
-    args = TrainingArguments(output_dir=out_dir, **config.training_args)
+    args              = TrainingArguments(output_dir=out_dir, **config.training_args)
 
-    trainer = Trainer(
-        model=model,
-        args=args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-        data_collator=data_collator,
-        compute_metrics=compute_metrics,
-    )
-
+    trainer           = Trainer (
+                                    model           = model,
+                                    args            = args,
+                                    train_dataset   = train_dataset,
+                                    eval_dataset    = eval_dataset,
+                                    data_collator   = data_collator,
+                                    compute_metrics = compute_metrics,
+                                )
     trainer.train()
 
 
