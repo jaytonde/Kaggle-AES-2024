4b2b6243143079ad57bbe3408fc3638281f261cb
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-05-17 15:38:38 +0530

    full fit file name changed

--- experiment.py
+++ experiment.py
@@ -92,7 +92,11 @@ def inference(config, trainer, eval_dataset, eval_df, out_dir):
     logits_df          = pd.DataFrame(logits, columns=[f"pred_{i}" for i in range(1, 7)])
     result_df          = pd.concat([eval_df, logits_df], axis=1)
 
-    file_path          = out_dir + '/' +f"fold_{config.fold}_oof.csv"
+    if config.full_fit:
+        file_path          = out_dir + '/' +"full_fit_oof.csv"
+    else:    
+        file_path          = out_dir + '/' +f"fold_{config.fold}_oof.csv"
+
     result_df.to_csv(file_path, index=False)
 
     print(f"OOF is saved at : {file_path}")
