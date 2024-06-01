239c02789682910e5bc792b954d1d2791051f623
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-06-01 14:40:20 +0530

    oigj

--- experiment_custom_model.py
+++ experiment_custom_model.py
@@ -82,7 +82,7 @@ def compute_loss(labels, num_classes, dist_matrix, logits):
     label_ids        = len(labels)*[[k for k in range(num_classes)]]
 
     distances        = [[float(dist_matrix[true_labels[j][i]][label_ids[j][i]]) for i in range(num_classes)] for j in range(len(labels))]
-    distances_tensor = torch.tensor(distances,device='cuda:0', requires_grad=True)
+    distances_tensor = torch.tensor(distances,requires_grad=True)
 
     err              = -torch.log(1-probas)*abs(distances_tensor)**2
     loss             = torch.sum(err,axis=1).mean()
