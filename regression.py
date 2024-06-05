76c11fe8ed1a896a9e4cf6337b99216df5050841
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-06-05 22:37:11 +0530

    added xlnet config and get optimizer function

--- regression.py
+++ regression.py
@@ -40,6 +40,37 @@ warnings.filterwarnings("ignore", category=UserWarning, module="transformers")
 load_dotenv()
 
 
+def get_optimizer_params(model, learning_rate = 0.0, weight_decay=0.0, type='s'):
+    param_optimizer = list(model.named_parameters())
+    no_decay        = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+    
+    if type   == 's':
+        optimizer_parameters = filter(lambda x: x.requires_grad, model.parameters())
+    elif type == 'i':
+        optimizer_parameters = [
+            {'params': [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay_rate': weight_decay},
+            {'params': [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay)],     'weight_decay_rate': weight_decay},
+            {'params': [p for n, p in model.named_parameters()       if "model" not in n],                    'weight_decay_rate': weight_decay, 'lr': learning_rate}
+        ]
+    elif type == 'a':
+        group1    = ['layer.0.','layer.1.','layer.2.','layer.3.']
+        group2    = ['layer.4.','layer.5.','layer.6.','layer.7.']    
+        group3    = ['layer.8.','layer.9.','layer.10.','layer.11.']
+        group_all = ['layer.0.','layer.1.','layer.2.','layer.3.','layer.4.','layer.5.','layer.6.','layer.7.','layer.8.','layer.9.','layer.10.','layer.11.']
+        optimizer_parameters = [
+            {'params': [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay) and not any(nd in n for nd in group_all)],'weight_decay_rate': weight_decay},
+            {'params': [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay) and any(nd in n for nd in group1)],       'weight_decay_rate': weight_decay, 'lr': learning_rate/2.6},
+            {'params': [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay) and any(nd in n for nd in group2)],       'weight_decay_rate': weight_decay, 'lr': learning_rate},
+            {'params': [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay) and any(nd in n for nd in group3)],       'weight_decay_rate': weight_decay, 'lr': learning_rate*2.6},
+            
+            {'params': [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay) and not any(nd in n for nd in group_all)],'weight_decay_rate': 0.0},
+            {'params': [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay) and any(nd in n for nd in group1)],       'weight_decay_rate': 0.0, 'lr': learning_rate/2.6},
+            {'params': [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay) and any(nd in n for nd in group2)],       'weight_decay_rate': 0.0, 'lr': learning_rate    },
+            {'params': [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay) and any(nd in n for nd in group3)],       'weight_decay_rate': 0.0, 'lr': learning_rate*2.6},
+            
+            {'params': [p for n, p in model.named_parameters() if "model" not in n], 'lr':learning_rate},
+        ]
+
 def get_model(config):
 
     tokenizer    = AutoTokenizer.from_pretrained(config.model_id)
